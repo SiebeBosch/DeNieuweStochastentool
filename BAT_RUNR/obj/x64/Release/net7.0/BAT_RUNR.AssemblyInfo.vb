5971dd5e0159eac6c1903f32f5faa32917a2cdd8
Siebe Bosch <siebe@hydroconsult.nl>
2024-08-06 16:31:44 +0200

    'bugfix_color_legend_webviewer'

--- BAT_RUNR/obj/x64/Release/net7.0/BAT_RUNR.AssemblyInfo.vb
+++ BAT_RUNR/obj/x64/Release/net7.0/BAT_RUNR.AssemblyInfo.vb
@@ -16,7 +16,7 @@ Imports System.Reflection
 <Assembly: System.Reflection.AssemblyCompanyAttribute("BAT_RUNR"),  _
  Assembly: System.Reflection.AssemblyConfigurationAttribute("Release"),  _
  Assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0"),  _
- Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05df1069ada3d5278224672789f2a93b41e00233"),  _
+ Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a17a85bfea8ded65fdcada281a909f6ff4ed9ff"),  _
  Assembly: System.Reflection.AssemblyProductAttribute("BAT_RUNR"),  _
  Assembly: System.Reflection.AssemblyTitleAttribute("BAT_RUNR"),  _
  Assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")> 
